ddaf6c495d7293f6f6797010ac9c95d456c6a894
jgruber <john.t.gruber@gmail.com>
2019-09-11 10:25:11 -0500

    made unit config listen on port 8105 - like iControlLX did, and made the config script use the unix-socket for config, not nginx listener

--- config/usr/bin/unitinit.py
+++ config/usr/bin/unitinit.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 from deepmerge import always_merger
 import glob
-import urllib2
+import socket
 import json
+
+UNIT_CONTROL_SOCKET = '/var/run/unit/control.sock'
 
 configuration = {}
 
@@ -14,8 +16,25 @@ for cf in sorted(glob.glob('/etc/unit/*.conf')):
 jsonconfig = json.dumps(configuration, indent=4, sort_keys=True)
 print('merged configuraion:')
 print(jsonconfig)
-request = urllib2.Request(url='http://localhost:8101/config/', data=jsonconfig)
-request.add_header('Content-Type', 'application/json')
-request.get_method = lambda: 'PUT'
-response = urllib2.urlopen(request)
-print('resp: %s' % response.read())
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+try:
+    sock.connect(UNIT_CONTROL_SOCKET)
+    message = "PUT /config HTTP/1.1\n"
+    message += "Host: localhost\n"
+    message += "Accept: */*\n"
+    message += "Content-Type: application/json\n"
+    message += "Content-Length: %s\n\n" % len(jsonconfig)
+    message += jsonconfig
+    sock.sendall(message)
+    res = [];
+    while True:
+        data = sock.recv(8192)
+        if not data:
+            break
+        res.append(data)
+    print('configuration response: %s' % ''.join(res))
+except socket.error as err:
+    print('error writing to unit control socket: %s' % err)
+finally:
+    sock.close()
