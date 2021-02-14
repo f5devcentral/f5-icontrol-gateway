6463007e2c94b888dd02cacdc0529bf8d02ec464
John Gruber <jgruber@f5.com>
2021-02-14 13:58:07 -0600

    including an example securing with X_API_KEY environment variable

---
+++ deploy_apikey_secured.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(dirname "$(readlink -f "$0")")
+
+cd $DIR
+
+if [ -z $X_API_KEY ]
+then
+    echo "Please export the X_API_KEY environment variable"
+    exit 1
+fi
+
+docker build -f Dockerfile_full_apikey -t f5-icontrol-gateway-apikey-secured .
+docker run --name f5-icontrol-gateway-apikey-secured --rm -i -t -p 8443:443 -v /tmp/logs/nginx:/var/log/nginx -e X_API_KEY=$X_API_KEY f5-icontrol-gateway-apikey-secured
+
+if [ -n "$XDG_RUNTIME_DIR" ]
+then
+   echo "opening browser with xdg-open"
+   sleep 2
+   xdg-open "https://localhost:8443"
+else
+   echo "available on https://localhost:8443"
+fi
+
+docker attach f5-icontrol-gateway-apikey-secured
